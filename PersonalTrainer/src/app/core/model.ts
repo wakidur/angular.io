15e0b2c8b4641cb21f42c7dc5a3aec864ab3ff29
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-09-27 19:40:26 +0600

    ffdfd

--- PersonalTrainer/src/app/core/model.ts
+++ PersonalTrainer/src/app/core/model.ts
@@ -11,9 +11,7 @@ export class WorkoutPlan {
         if (!this.exercises) {
             return 0;
         }
-
         const total = this.exercises.map((e) => e.duration).reduce((previous, current) => previous + current);
-
         return (this.restBetweenExercise ? this.restBetweenExercise : 0) * (this.exercises.length - 1) + total;
     }
 }
