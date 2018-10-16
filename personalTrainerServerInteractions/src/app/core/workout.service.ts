211252bc2e45bc7e3559636d83e2fe866688f884
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-10-16 09:23:21 +0600

    #ff aa ss

---
+++ personalTrainerServerInteractions/src/app/core/workout.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { _throw } from "rxjs/observable/throw";
+import { of } from "rxjs/observable/of";
+import { forkJoin } from "rxjs/observable/forkJoin";
+import { map, catchError } from "rxjs/operators";
+
+import { CoreModule } from "./core.module";
+import { Exercise, ExercisePlan, WorkoutPlan } from "./model/model";
+
+
+@Injectable({
+  providedIn: CoreModule
+})
+export class WorkoutService {
+  /**
+   * GET /databases/{database}/collections
+   * Example:
+   * https://api.mlab.com/api/1/databases/my-db/collections?apiKey=myAPIKey
+   */
+  workouts: Array<WorkoutPlan> = [];
+  exercises: Array<Exercise> = [];
+  workout: WorkoutPlan;
+  collectionsUrl = "https://api.mlab.com/api/1/databases/training/collections";
+  apiKey = "TRZfI48FK_XQeAyB5EE5-7z3d8wFgcgV";
+  params = "?apiKey=" + this.apiKey;
+
+  constructor(public httpClient: HttpClient) {}
+
+  getExercises(): Observable<Exercise[]> {
+    return this.httpClient
+      .get<Exercise>(this.collectionsUrl + "/exercises" + this.params)
+      .pipe(catchError(this.handleError("getExercises", [])));
+  }
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: HttpErrorResponse): Observable<T> => {
+      if (error.status === 404) {
+        console.log("HTTP 404 Not found error");
+        return of(result as T);
+      } else {
+        console.error(error);
+        return _throw("An error occurred:", error.error.message);
+      }
+    };
+  }
+}
