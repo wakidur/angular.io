586a46b8bedbc31670b61fec2555ed22af8a5944
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-10-31 15:49:26 +0600

    #ff ssssf

---
+++ chapterSixTrainer/src/app/shared/busy-indicator.directive.1.ts
@@ -0,0 +1,66 @@
+import { Directive, HostBinding, ElementRef, Renderer, OnDestroy, AfterViewInit  } from "@angular/core";
+import { NgModel } from "@angular/forms";
+
+@Directive({
+  selector: "[appBusyIndicator][ngModel]"
+})
+export class BusyIndicatorDirective implements  AfterViewInit , OnDestroy {
+  // constructor(private model: NgModel) {}
+  // Opthinal decorator,
+  constructor(
+    private model: NgModel,
+    private element: ElementRef,
+    private renderer: Renderer
+  ) {}
+
+  private subscriptions: Array<any> = [];
+
+  ngAfterViewInit() {
+    this.subscriptions.push(
+      this.model.control.statusChanges.subscribe((status: any) => {
+        if (this.model.control.pending) {
+          this.renderer.setElementStyle(
+            this.element.nativeElement,
+            "border-width",
+            "3px"
+          );
+          this.renderer.setElementStyle(
+            this.element.nativeElement,
+            "border-color",
+            "red"
+          );
+        } else {
+          this.renderer.setElementStyle(
+            this.element.nativeElement,
+            "border-width",
+            null
+          );
+          this.renderer.setElementStyle(
+            this.element.nativeElement,
+            "border-color",
+            null
+          );
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
+  private get validating(): boolean {
+    return this.model.control != null && this.model.control.pending;
+  }
+
+  @HostBinding("style.borderWidth")
+  get controlBorderWidth(): string {
+    return this.validating ? "3px" : null;
+  }
+
+  @HostBinding("style.borderColor")
+  get controlBorderColor(): string {
+    return this.validating ? "gray" : null;
+  }
+}
--- chapterSixTrainer/src/app/shared/busy-indicator.directive.ts
+++ chapterSixTrainer/src/app/shared/busy-indicator.directive.ts
@@ -1,55 +1,16 @@
-import { Directive, HostBinding, ElementRef, Renderer, OnDestroy, AfterViewInit  } from "@angular/core";
+import { Directive, HostBinding  } from "@angular/core";
 import { NgModel } from "@angular/forms";
 
 @Directive({
   selector: "[appBusyIndicator][ngModel]"
 })
-export class BusyIndicatorDirective implements  AfterViewInit , OnDestroy {
+export class BusyIndicatorDirective  {
   // constructor(private model: NgModel) {}
   // Opthinal decorator,
   constructor(
-    private model: NgModel,
-    private element: ElementRef,
-    private renderer: Renderer
+    private model: NgModel
   ) {}
 
-  private subscriptions: Array<any> = [];
-
-  ngAfterViewInit() {
-    this.subscriptions.push(
-      this.model.control.statusChanges.subscribe((status: any) => {
-        if (this.model.control.pending) {
-          this.renderer.setElementStyle(
-            this.element.nativeElement,
-            "border-width",
-            "3px"
-          );
-          this.renderer.setElementStyle(
-            this.element.nativeElement,
-            "border-color",
-            "red"
-          );
-        } else {
-          this.renderer.setElementStyle(
-            this.element.nativeElement,
-            "border-width",
-            null
-          );
-          this.renderer.setElementStyle(
-            this.element.nativeElement,
-            "border-color",
-            null
-          );
-        }
-      })
-    );
-  }
-
-  ngOnDestroy(): void {
-    // Called once, before the instance is destroyed.
-    // Add 'implements OnDestroy' to the class.
-    this.subscriptions.forEach((s) => s.unsubscribe());
-  }
   private get validating(): boolean {
     return this.model.control != null && this.model.control.pending;
   }
@@ -63,12 +24,4 @@ export class BusyIndicatorDirective implements  AfterViewInit , OnDestroy {
   get controlBorderColor(): string {
     return this.validating ? "gray" : null;
   }
-
-  // @HostBinding("readOnly") get busy() {
-  //   return this.isbusy;
-  // }
-
-  // @HostBinding("attr.disable") get canEdit(): string {
-  //   return !this.isAdmin? "disable" : null
-  // }
 }
