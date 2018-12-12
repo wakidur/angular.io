c02abe49304ede890f048399083833bffc32da42
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-12-12 16:40:44 +0600

    #ddaa

--- trainer/trainerVersionTwo/src/app/workout-runner/workout-runner.module.ts
+++ trainer/trainerVersionTwo/src/app/workout-runner/workout-runner.module.ts
@@ -4,12 +4,14 @@ import { WorkoutRunnerComponent } from "./workout-runner.component";
 
 @NgModule({
   declarations: [
+    // adding a component/directive/pipe to the declaration section of a module makes them available inside the module.
     WorkoutRunnerComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    // we export the component/directive/pipe that it becomes available to be used across modules.
     WorkoutRunnerComponent
   ]
 })
