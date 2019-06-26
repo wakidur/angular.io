0fa9ea60b97707c698c6444550639e164a122a9e
wakidur <wakidur@gmail.com>
2019-06-26 10:22:49 +0600

    #angular guidline add

---
+++ angular-guidelines-v8/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ProductListComponent } from './shop/product-list/product-list.component';
+import { TopBarComponent } from './shop/top-bar/top-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
